75fbc49e13aa1c1a9d5332683781d20fa6fead3f
Taylor Gulley <taylorlgulley@yahoo.com>
2018-09-10 14:44:56 -0500

    added the database and login

--- trivia-app/src/index.js
+++ trivia-app/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import TriviaApp from './TriviaApp';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <TriviaApp />
+    </Router>
+    , document.getElementById('root'));
 registerServiceWorker();
