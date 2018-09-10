9028c236a1f9998394af5c62ecfbedb78f5a560d
Taylor Gulley <taylorlgulley@yahoo.com>
2018-09-10 15:54:49 -0500

    got the navbar added with some routes

---
+++ trivia-app/src/Components/navbar/NavBar.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+class NavBar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-light fixed-top light-blue flex-md-nowrap p-0 shadow">
+                <ul className="nav nav-pills">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/categories">Categories</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/quiz">Take Quiz</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/login">Logout</Link>
+                    </li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default NavBar
--- trivia-app/src/TriviaApp.js
+++ trivia-app/src/TriviaApp.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import ApplicationViews from './Components/ApplicationViews'
+import NavBar from './Components/navbar/NavBar'
 import './App.css';
 
 class TriviaApp extends Component {
   render() {
     return (
       <React.Fragment>
+        <NavBar />
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
