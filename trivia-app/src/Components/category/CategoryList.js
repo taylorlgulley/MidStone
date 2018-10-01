61ad04a5c1183bc854d861806d983619c278605a
Taylor Gulley <taylorlgulley@yahoo.com>
2018-09-30 22:02:55 -0500

    fixed more styling

--- trivia-app/src/Components/category/CategoryList.js
+++ trivia-app/src/Components/category/CategoryList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import AddCategoryModal from './AddCategoryModal'
 import { Link } from 'react-router-dom'
 import { Button } from 'reactstrap';
+import edit from '../../images/edit.png'
 import './Category.css'
 
 export default class CategoryList extends Component {
@@ -16,7 +17,7 @@ export default class CategoryList extends Component {
                             <div className="card-body">
                                 <h2>{category.name}</h2>
                                 <h4>
-                                    <Link className="nav-link path" to={`/questionlist/${category.id}`}>Add Questions</Link>
+                                    <Link className="nav-link path" to={`/questionlist/${category.id}`}><img className="edit-image" alt="edit-icon" src={edit}/> Add Questions</Link>
                                 </h4>
                                 <Button onClick={() => this.props.deleteCategory(category.id)}>Delete Category</Button>
                             </div>
--- trivia-app/src/Components/questions/QuestionList.js
+++ trivia-app/src/Components/questions/QuestionList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import AddQuestionModal from './AddQuestionModal'
 import QuestionCard from './QuestionCard'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Question.css'
 
 export default class CategoryList extends Component {
@@ -10,11 +11,13 @@ export default class CategoryList extends Component {
             <React.Fragment>
                 <div className="question-list">
                     <AddQuestionModal {...this.props}/>
-                    {
-                        this.props.questions.filter(question => question.categoryId === parseInt(this.props.match.params.categoryId, 10)).map(question =>
-                            <QuestionCard className="question-style" key={question.id} question={question} {...this.props}/>
-                        )
-                    }
+                    <div className="row flex-wrap justify-content-around question-section">
+                        {
+                            this.props.questions.filter(question => question.categoryId === parseInt(this.props.match.params.categoryId, 10)).map(question =>
+                                <QuestionCard className="card" key={question.id} question={question} {...this.props}/>
+                            )
+                        }
+                    </div>
                 </div>
             </React.Fragment>
         )
